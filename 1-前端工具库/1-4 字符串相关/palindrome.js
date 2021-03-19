8fc052000137e2483f7009d944e027e61f115a48
zhang.yizhe@northeastern.edu <zhang.yizhe@northeastern.edu>
2021-03-19 17:34:45 -0400

    move tools to algJS

--- 1-前端工具库/1-4 字符串相关/palindrome.js
+++
@@ -1,9 +0,0 @@
-function palindrome(str){
-    let arr = [...str];
-    arr.reverse();
-    return arr.join("") === str;
-}
-
-// test
-let str = "lol";
-console.log(palindrome(str));
--- 1-前端工具库/1-4 字符串相关/truncate.js
+++
@@ -1,8 +0,0 @@
-// 超出部分用...
-function truncate(str,size){
-    return str.slice(0,size) + "...";
-}
-
-// test
-let str = "abcdefghijqqqqqqqqqqqqqqqq";
-console.log(truncate(str,10));
